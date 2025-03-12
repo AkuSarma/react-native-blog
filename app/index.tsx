36e420e9d50ec6abc5b69edb536cdac2f5ab59a8
Aku Sarma <akusarma1702@gmail.com>
2025-03-12 11:21:29 +0530

    feat: integrate Supabase for authentication and add Auth component

--- app/index.tsx
+++ app/index.tsx
@@ -1,20 +1,32 @@
-import React from 'react';
-import { Text, View, Button } from 'react-native';
+import 'react-native-url-polyfill/auto'
+import { useState, useEffect } from 'react'
+import { supabase } from '../lib/supabase'
+import Auth from '../components/Auth'
+import { View, Text, Button } from 'react-native'
+import { Session } from '@supabase/supabase-js'
 import { useRouter } from 'expo-router';
 
-export default function Index() {
+export default function App() {
   const router = useRouter();
+  const [session, setSession] = useState<Session | null>(null)
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+    })
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+  }, [])
 
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
+    <View>
+      <Auth />
+      {session && session.user && <Text>{session.user.id}</Text>}
+
       <Text>Welcome to the Blog App</Text>
-      <Button title="Get Started" onPress={() => router.push('/home')} />
+      <Button title="Get Started" onPress={() => router.push("/home")} />
     </View>
   );
 }
